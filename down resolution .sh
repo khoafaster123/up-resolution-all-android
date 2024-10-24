3215c50cb22c4b824f1f1d4db1eba17e679b5051
KhoaGame <96251326+khoafaster123@users.noreply.github.com>
2024-10-24 22:56:04 +0700

    down resolution HD
    down your dislay phone resolution to HD

---
+++ down resolution .sh
@@ -0,0 +1,20 @@
+-------------------------
+|                       |
+|  DEV:KhoaGame         |
+|  Youtube:KhoaGame     |
+|                       |
+|                       |
+-------------------------
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "down your dislay phone resolution hd"
+echo "đang giảm độ phâng giải màn hình của bạn xuống hd"
+sleep 5
+wm size 780x1280
+sleep 5
+echo "done"
+echo "đã thành công "
+sleep 2
